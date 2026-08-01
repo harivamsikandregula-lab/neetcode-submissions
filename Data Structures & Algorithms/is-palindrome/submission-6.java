029dc6ab44d9488eef80a402e26ae4407612d2f3
KANDREGULA HARI VAMSI <harivamsikandregula@gmail.com>
2026-08-01 13:02:15 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length()==1) return true;
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(i<j){
+                char left=Character.toLowerCase(s.charAt(i));
+                char right=Character.toLowerCase(s.charAt(j));
+                if(left!=right) return false;
+                else{
+                    i++;
+                    j--;
+                }
+            }
+        }
+        return true;
+    }
+}
